8f7374a07d836ab60542a3beb95ac0225c320086
drabinowitz <dmitrirabinowitz+development@gmail.com>
2014-10-29 18:23:42 -0400

    passing shared functional tests

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,9 +1,65 @@
 var makeQueue = function(){
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+ 
+  //declare results variable
+  var results = {};
+
+  //provide size variable
+  results.enqueuePosition = 0;
+  results.dequeuePosition = 0;
+
+  //provide a storage variable
+  results.storage = {};
+
+  extend(results, queueMethods);
+
+  //returns results
+  return results;
 };
 
-var queueMethods = {};
+//provide extend function
+
+var extend = function(to,from){
+
+  for (var item in from){
+
+    to[item] = from[item];
+
+  }
+};
 
 
 
+  var queueMethods = {};
+
+  // Implement the methods below
+
+  queueMethods.enqueue = function(value){
+    //write value to storage at size
+    this.storage[this.enqueuePosition] = value;
+    
+    //increment size
+    this.enqueuePosition++;
+
+    //return the value after storing it
+    return value;
+  };
+
+  queueMethods.dequeue = function(){
+    //results variable for returning data to user
+    var results;
+    if (this.size() > 0) {
+      results = this.storage[this.dequeuePosition];
+      delete this.storage[this.dequeuePosition];
+      this.dequeuePosition++;
+    }
+    //return data to user
+    //decrease size
+    return results;
+  };
+
+  queueMethods.size = function(){
+    //return size
+    return (this.enqueuePosition - this.dequeuePosition);
+  };
--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,63 @@
 var makeStack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+  //declare results variable
+  var results = {};
+
+  //provide size variable
+  results.length = 0;
+
+  //provide a storage variable
+  results.storage = {};
+
+  extend(results, stackMethods);
+
+  //returns results
+  return results;
 };
+
+//provide extend function
+
+var extend = function(to,from){
+
+  for (var item in from){
+
+    to[item] = from[item];
+
+  }
+
+}
 
 var stackMethods = {};
 
+//store methods for stack in stackMethods
 
+stackMethods.push = function(value){
+  //use this keyword
+  this.storage[this.length] = value;
+  this.length++;
+  return value;
+}
+
+stackMethods.pop = function(){
+  var results;
+
+  if(this.length > 0){
+  
+    this.length--;
+    
+    results = this.storage[this.length];
+  
+    delete this.storage[this.length];
+  
+  }
+  
+  return results;
+}
+
+stackMethods.size = function(){
+
+  return this.length;
+
+}
