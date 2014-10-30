638d5ec86e95f358a93d2f7308e45bcf31e1654d
drabinowitz <dmitrirabinowitz+development@gmail.com>
2014-10-30 19:03:00 -0400

    tree tests passing

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,20 +1,54 @@
 var makeTree = function(value){
   var newTree = {};
   newTree.value = value;
-  newTree.children = undefined;
+  newTree.children = [];
+  extend(newTree,treeMethods);
   return newTree;
 };
 
 
+var extend = function(to,from){
 
+  for (var item in from){
+
+    to[item] = from[item];
+
+  }
+};
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value){
 
+  var node = makeTree(value);
+
+  this.children.push(node);
+
 };
 
 treeMethods.contains = function(target){
+
+  var result = false;
+
+  var recursiveContains = function(currentNode){
+
+    result = result || currentNode.value === target;
+
+    if(currentNode.children.length > 0){
+
+      for (var i = 0; i < currentNode.children.length; i++){
+
+        recursiveContains( currentNode.children[i] );
+
+      }
+
+    }
+
+  }
+
+  recursiveContains(this);
+
+  return result;
 
 };
 
