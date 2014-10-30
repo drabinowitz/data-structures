c882e010dc271de2e6803497e8695a9547090687
drabinowitz <dmitrirabinowitz+development@gmail.com>
2014-10-30 18:40:39 -0400

    passing linkedList test

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,44 @@ var makeLinkedList = function(){
   list.tail = null;
 
   list.addToTail = function(value){
+    if (list.tail === null) {
+
+      list.head = {};
+      list.head.value = value;
+      list.tail = list.head;
+    } else {
+      list.tail.linkedListNode = {};
+      list.tail = list.tail.linkedListNode;
+      list.tail.value = value;
+    }
+    return value;
   };
 
   list.removeHead = function(){
+    var results;
+    if (list.head.hasOwnProperty("linkedListNode")) {
+      results = list.head.value;
+      list.head = list.head.linkedListNode;
+    } else if (list.head !== null) {
+      results = list.head.value;
+      list.head = null;
+      list.tail = null;
+    }
+    return results;
   };
 
   list.contains = function(target){
+    var results = false;
+    var lLNode;   
+    if(list.head !== null){
+      results = list.head.value === target;
+      lLNode = list.head;
+      while(!results && lLNode.hasOwnProperty("linkedListNode")) {
+        lLNode = lLNode.linkedListNode;
+        results = lLNode.value === target;
+      }
+    }
+    return results;
   };
 
   return list;
