28a9308508272daed9c5bb7a952f2ae2aecbbff6
drabinowitz <dmitrirabinowitz+development@gmail.com>
2014-10-30 23:01:40 -0400

    passing binary search tree tests

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,6 +1,44 @@
 var makeBinarySearchTree = function(value){
+  var bst = Object.create(binarySearchMethods);
+
+  bst.value = value;
+  bst.left = null;
+  bst.right = null;
+  
+  return bst;
 };
 
+var binarySearchMethods = {};
+
+binarySearchMethods.insert = function(value){
+
+  var insert = value < this.value ? "left" : "right";
+
+  if (this[insert] === null) {
+    this[insert] = makeBinarySearchTree(value);
+  } else {
+    this[insert].insert(value);
+  } 
+
+};
+
+binarySearchMethods.contains = function(target) {
+
+  var insert = target < this.value ? "left" : "right";
+
+  var results = false;
+
+  if (this.value === target) {
+    return results = true;
+  } else if (this[insert] !== null) {
+    results = this[insert].contains(target);
+  }
+  return results;
+};
+
+binarySearchMethods.depthFirstLog = function(callback) {
+
+};
 
 /*
  * Complexity: What is the time complexity of the above functions?
