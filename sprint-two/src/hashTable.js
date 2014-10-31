a2fd6523883d99a0f1553948ce34303c85119add
drabinowitz <dmitrirabinowitz+development@gmail.com>
2014-10-31 13:22:06 -0400

    passing all hash tests

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -3,29 +3,75 @@ var HashTable = function(){
   this._storage = makeLimitedArray(this._limit);
 };
 
+function arrayAtIndexLookup(arrayAtIndex,k){
+
+  var keyInArray = false;
+
+  if (Array.isArray(arrayAtIndex)) {
+    keyInArray = -1;
+    for (var j = 0; j < arrayAtIndex.length; j++) {
+      if (arrayAtIndex[j][0] === k) {
+        keyInArray = j;
+      }
+    }
+  }
+
+  return keyInArray;
+
+}
+
 HashTable.prototype.insert = function(k, v){
   var i = getIndexBelowMaxForKey(k, this._limit);
 
-  while (this._storage.get(i) !== undefined) {
-    i++;
+  var arrayAtIndex = this._storage.get(i);
+
+  var keyInArray = arrayAtIndexLookup(arrayAtIndex,k);
+
+  if (keyInArray === false){
+
+    arrayAtIndex = [];
+    arrayAtIndex.push([k, v]);
+
+  } else if (keyInArray === -1){
+
+    arrayAtIndex.push([k, v]);
+    
+  } else {
+
+    arrayAtIndex[keyInArray][1] = v;
+    
   }
-
-  this._storage.set(i, v);
-
-  return v;
+ 
+  this._storage.set(i, arrayAtIndex);
 
 };
 
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
 
-  return this._storage.get(i);
+  var result = null;
+
+  var arrayAtIndex = this._storage.get(i);
+
+  var keyInArray = arrayAtIndexLookup(arrayAtIndex,k);
+
+  if (keyInArray !== false && keyInArray >= 0){
+
+    result = arrayAtIndex[keyInArray][1];
+
+  }
+
+  return result;
 
 };
 
 HashTable.prototype.remove = function(k){
   
-  this.insert(k, undefined);
+  if (this.retrieve(k) !== null){
+
+    this.insert(k,null);
+
+  }
 
 };
 
