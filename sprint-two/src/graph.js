4270c2a4ad02c8539e88375bf9dc2b14ff8b5664
drabinowitz <dmitrirabinowitz+development@gmail.com>
2014-10-31 17:51:36 -0400

    passing graph tests

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -1,22 +1,83 @@
 var Graph = function(){
+  this._nodes = {};
+  this._nodeCount = 0;
 };
 
 Graph.prototype.addNode = function(newNode, toNode){
+
+  if (this._nodeCount === 1) {
+    for (var item in this._nodes) {
+      if (this._nodes.hasOwnProperty(item)) {
+        toNode = item;
+      }
+    }
+  }
+  this._nodes[newNode] = {};
+  this._nodes[newNode].edges = {};
+  this._nodes[newNode].edgeCount = 0;
+
+  if (toNode) {
+    this.addEdge(newNode, toNode);
+  }
+  this._nodeCount++;
 };
 
 Graph.prototype.contains = function(node){
+  var result = false;
+  for (var item in this._nodes){
+
+    result = result || item === node;
+
+  }
+  return result;
 };
 
 Graph.prototype.removeNode = function(node){
+  if (this._nodes[node]){
+    for (var item in this._nodes[node].edges){
+      this._nodes.removeEdge(node,item);
+    }
+    delete this._nodes[node];
+  }
 };
 
 Graph.prototype.getEdge = function(fromNode, toNode){
+
+  var results = false;
+  if (this._nodes[toNode].edges[fromNode]) {
+    results = true;
+  }
+
+  return results;
 };
 
 Graph.prototype.addEdge = function(fromNode, toNode){
+
+  if (this._nodes[fromNode] && this._nodes[toNode]) {
+
+    this._nodes[fromNode].edges[toNode] = this._nodes[toNode];
+    this._nodes[toNode].edges[fromNode] = this._nodes[fromNode];
+
+    this._nodes[fromNode].edgeCount++;
+    this._nodes[toNode].edgeCount++;
+  }
+
 };
 
 Graph.prototype.removeEdge = function(fromNode, toNode){
+
+  if (this.getEdge(fromNode, toNode)) {
+    delete this._nodes[fromNode].edges[toNode];
+    delete this._nodes[toNode].edges[fromNode];
+    
+    this._nodes[fromNode].edgeCount--;
+    this._nodes[toNode].edgeCount--;
+  
+    this._nodes[fromNode].edgeCount === 0 && delete this._nodes[fromNode];  
+    this._nodes[toNode].edgeCount === 0 && delete this._nodes[toNode];  
+  }
+
+
 };
 
 /*
